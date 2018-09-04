d5f195b5410d2241979fcc47478c51b798e91176
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-09-04 20:43:03 +0530

    added testquestion file

--- testcase/test_askquestion.py
+++ testcase/test_askquestion.py
@@ -17,93 +17,75 @@ class TestAskquestionSurvey(unittest.TestCase):
     def test_askquestion(self):
         print("start question on survey")
         self.question.first_question(first_question_enter)
+        result = self.question.valid_first_question()
+        assert result == str(first_question_enter)
 
     @pytest.mark.run(order=5)
     def test_question_sec(self):
-        print("question_second ")
+        print("question second ")
         self.question.second_question(second_question_enter)
+        result = self.question.valid_second_question()
+        assert result == str(second_question_enter)
 
     @pytest.mark.run(order=6)
     def test_question_third(self):
-        print("question_third ")
+        print("question third ")
         self.question.third_question(third_question_enter)
+        result = self.question.valid_third_question()
+        assert result == str(third_question_enter)
 
     @pytest.mark.run(order=7)
     def test_question_four(self):
-        print("question_four ")
+        print("question four ")
         self.question.four_question(four_question_enter)
+        result = self.question.valid_four_question()
+        assert result == str(four_question_enter)
 
     @pytest.mark.run(order=8)
     def test_question_five(self):
-        print("question_five ")
+        print("question five ")
         self.question.five_question(five_question_enter)
+        result = self.question.valid_five_question()
+        assert result == str(five_question_enter)
 
     @pytest.mark.run(order=9)
     def test_question_six(self):
-        print("question_six ")
+        print("question six ")
         self.question.six_question(six_question_enter)
+        result = self.question.valid_six_question()
+        assert result == str(six_question_enter)
 
     @pytest.mark.run(order=10)
     def test_question_seven(self):
-        print("question_seven ")
+        print("question seven ")
         self.question.seven_question(seven_question_enter)
+        result = self.question.valid_seven_question()
+        assert result == str(seven_question_enter)
 
     @pytest.mark.run(order=11)
     def test_question_eight(self):
-        print("question_eight ")
+        print("question eight ")
         self.question.eight_question(eight_question_enter)
+        result = self.question.valid_eight_question()
+        assert result == str(eight_question_enter)
 
     @pytest.mark.run(order=12)
     def test_question_nine(self):
-        print("question_nine ")
+        print("question nine ")
         self.question.nine_question(nine_question_enter)
+        result = self.question.valid_nine_question()
+        assert result == str(nine_question_enter)
 
     @pytest.mark.run(order=13)
     def test_question_ten(self):
-        print("question_second ")
+        print("question second ")
         self.question.ten_question(ten_question_enter)
-
-        # result = self.question.valid_all_question()
-        # assert result == True
-
-
-
-
-    # @pytest.mark.run(order=14)
-    # def valid_all_question(self):
-    #     try:
-    #        result = self.question.valid_first_question()
-    #        assert (result == True)
-    #
-    #     except Exception as e:
-    #        print("Question Adding Failed" + str(e))
-    #
-    #    # self.question.valid_second_question()
-        # assert (result == True)
-
-        # self.question.valid_third_question()
-        # assert (result == True)
-        #
-        # self.question.valid_four_question()
-        # assert (result == True)
-        #
-        # self.question.valid_five_question()
-        # assert (result == True)
-        #
-        # self.question.valid_six_question()
-        # assert (result == True)
-        #
-        # self.question.valid_seven_question()
-        # assert (result == True)
-        #
-        # self.question.valid_eight_question()
-        # assert (result == True)
-        #
-        # self.question.valid_nini_question()
-        # assert (result == True)
-        #
-        # self.question.valid_ten_question()
-        # result = self.ss.valid_page_title()
-        # assert result == True
-
-
+        result = self.question.valid_ten_question()
+        assert result == str(ten_question_enter)
+    #drag and drop--------------------------------------------------------------
+    @pytest.mark.run(order=14)
+    def test_question_drag(self):
+        print("drag and drop ")
+        self.question.drag_drop_first(four_question_enter)
+        result = self.question.valid_four_question()
+        assert result == str(four_question_enter)
